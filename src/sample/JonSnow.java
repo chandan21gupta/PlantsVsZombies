710ef3200d4baabd69a7e5ba3aa33d746a9cab3a
kashinkoji <chandan18386@iiitd.ac.in>
2019-11-29 18:54:06 +0530

    Image view made clickable

--- src/sample/JonSnow.java
+++ src/sample/JonSnow.java
@@ -3,32 +3,25 @@ package sample;
 public class JonSnow implements Plant{
 
     private float health = 100;
-    private float damage = 15;
+    private float damage = 100;
     private final String img = "@../images/chars/johnSnow.png";
 
     public float getHealth() {
         return this.health;
     }
-
     public void setHealth(float health) {
-        this.health = this.health - health;
+        this.health = health;
     }
+    public void attack() {
 
-    public void attack(Zombies Z) {
-        Z.setHealth(damage);
     }
-
     public float getDamage() {
         return this.damage;
     }
-
     public void setDamage(float damage) {
         this.damage = damage;
     }
-
     public String getName() {
         return this.img;
     }
-
-
 }
--- src/sample/Level_One_Controller.java
+++ src/sample/Level_One_Controller.java
@@ -1,7 +1,41 @@
 package sample;
 
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+import java.io.FileInputStream;
+
 public class Level_One_Controller {
-    public void generatePlant() {
-        System.out.println("Generating Plant");
+    String current_plant = null;
+    public void generatePlant(MouseEvent event) {
+       current_plant = event.getPickResult().getIntersectedNode().getId();
+    }
+
+    public void addPlant(MouseEvent event) {
+        if(current_plant == null)
+            return;
+        String img_url = null;
+        Image image = null;
+        if(current_plant.equals("jonSnow")) {
+            Plant jonSnow = new JonSnow();
+            img_url = jonSnow.getName();
+            System.out.println(img_url);
+        }
+        try {
+            image = new Image("file:src/images/chars/johnSnow.png");
+        }
+        catch(Exception e) {
+            //do nothing
+        }
+        ImageView imageView = new ImageView(image);
+        double x = event.getX();
+        double y = event.getY();
+        imageView.setX(x);
+        imageView.setY(y);
+        System.out.println(x+" "+y);
+        current_plant = null;
     }
 }
